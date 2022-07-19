cbcf47016dc847e7ee63f8988edb16de6b674e9d
Christian Sanchez <christian.s.molina@gmail.com>
2022-07-19 15:05:29 -0500

    Hooks added

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,57 @@
+// Common
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+// Services
+import { auth } from '@services/auth';
+
+const useAuth = () => {
+    const router = useRouter();
+
+    const [user, setUser] = useState(null);
+    const [isAuth, setIsAuth] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        ( () => {
+            setIsAuth(localStorage.getItem('token') !== null);
+            setUser(JSON.parse(localStorage.getItem('user')));
+        } )();
+        return () => { };
+    }, []);
+
+    const authReq = async (form) => {
+        try {
+            setLoading(true);
+            const response = await auth(form);
+            localStorage.setItem('user', JSON.stringify(response.data.data));
+            localStorage.setItem('token', JSON.stringify(response.data.token));
+            router.replace('/home');
+        } catch (error) {
+            setError(error.response.data);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const logoutReq = async () => {
+        try {
+            localStorage.removeItem('token');
+            localStorage.removeItem('user');
+            router.replace('/');
+        } catch (error) {
+            setError(error.response.data);
+        }
+    };
+
+    return {
+        user,
+        isAuth,
+        authReq,
+        logoutReq,
+        loading,
+        error,
+    };
+};
+
+export default useAuth;
