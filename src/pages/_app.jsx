76b8a115d9deec6f3a1d1b9fc1767592e24b66d0
Christian Sanchez <christian.s.molina@gmail.com>
2022-07-20 10:33:44 -0500

    RouteProtector added

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,14 @@
+// Styles
 import '../styles/globals.sass'
+// RouteProtector
+import RouteProtector from '@layouts/RouteProtector'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <RouteProtector>
+            <Component {...pageProps} />
+        </RouteProtector>
+    )
 }
 
 export default MyApp
