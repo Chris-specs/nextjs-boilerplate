d049acbe28ade502c743432fda56c36bfef73335
Christian Sanchez <christian.s.molina@gmail.com>
2022-07-22 10:09:29 -0500

    getStaticsProps added to pages

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -26,4 +26,10 @@ const HomePage = () => {
 
 HomePage.protected = true
 
+export async function getStaticProps(context) {
+    return {
+        props: {}, // will be passed to the page component as props
+    }
+}
+
 export default HomePage
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -21,4 +21,10 @@ const RootPage = () => {
     )
 }
 
+export async function getStaticProps(context) {
+    return {
+        props: {}, // will be passed to the page component as props
+    }
+}
+
 export default RootPage
